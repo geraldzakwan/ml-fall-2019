a0b6957f570595ee8c2205736ecb0f25b51e7bc5
geraldzakwan <geraldi.dzakwan@gmail.com>
2019-10-02 22:23:12 -0400

    Finish 2 b,c,d but with bug error_rate = 0.6

--- hw_2/Q2.py
+++ hw_2/Q2.py
@@ -32,6 +32,31 @@ def calculate_params(feature_matrix, label_vector):
 
     return np.dot(np.dot(inverse_matmul, feature_matrix_transposed), label_vector)
 
+def predict_values(model_params, feature_matrix):
+    return np.dot(feature_matrix, model_params)
+
+def calculate_squared_loss(prediction_values, label_vector, average=True):
+    total_squared_loss = np.sum((prediction_values - label_vector) ** 2)
+
+    if average:
+        return total_squared_loss / len(label_vector)
+
+    return total_squared_loss
+
+def predict_label(predict_values, threshold):
+    return [1.0 if value > threshold else 0.0 for value in predict_values]
+
+def calculate_error_rate(predictions, label_vector):
+    verdicts = [1 if y_pred == y else 0 for y_pred, y in zip(predictions, label_vector)]
+
+    return np.sum(verdicts) / len(verdicts)
+
+def calculate_false_positive_rate(predictions, label_vector):
+    total_false_positives = np.sum([1 if y_pred == 1.0 and y == 0.0 else 0 for y_pred, y in zip(predictions, label_vector)])
+    total_negatives = np.sum([1 if y == 0.0 else 0 for y in label_vector])
+
+    return total_false_positives / total_negatives
+
 if __name__=='__main__':
     columns, train_data = load_csv_to_ndarray('data/compas-train.csv')
 
@@ -40,4 +65,20 @@ if __name__=='__main__':
 
     ols_params = calculate_params(x_train, y_train)
 
-    print(ols_params)
+    columns, test_data = load_csv_to_ndarray('data/compas-test.csv')
+
+    x_test = create_feature_matrix(test_data)
+    y_test = create_label_vector(test_data)
+
+    prediction_values = predict_values(ols_params, x_test)
+
+    average_squared_loss = calculate_squared_loss(prediction_values, y_test)
+    print('Test averaged squared loss: ' + str(average_squared_loss))
+
+    predictions = predict_label(prediction_values, 0.5)
+
+    test_error_rate = calculate_error_rate(predictions, y_test)
+    print('Test error rate: ' + str(test_error_rate))
+
+    test_false_positive_rate = calculate_false_positive_rate(predictions, y_test)
+    print('Test false positive rate: ' + str(test_false_positive_rate))
