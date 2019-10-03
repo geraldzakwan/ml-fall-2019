67a17be67b1d78a43b0c2dc7e279bd0068290fc5
geraldzakwan <geraldi.dzakwan@gmail.com>
2019-10-03 16:53:21 -0400

    Linear regression done

--- hw_2/helper.py
+++ hw_2/helper.py
@@ -1,6 +1,18 @@
 import pandas as pd
+import numpy as np
 
 class Helper():
     @staticmethod
     def load_csv(filepath):
         return pd.read_csv(filepath)
+
+    @staticmethod
+    def load_csv_to_ndarray(filepath):
+        with open(filepath, 'r') as f:
+            header = f.readline()
+
+        # Read using numpy lib
+        data = np.genfromtxt(filepath, dtype=float, delimiter=',')
+
+        # Return all rows except the first one
+        return header.strip('\n').split(','), data[1:len(data)]
