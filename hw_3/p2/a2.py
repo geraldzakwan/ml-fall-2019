39f45e838112b8830617f1c6f8d672f915072855
geraldzakwan <geraldi.dzakwan@gmail.com>
2019-11-01 03:10:05 -0400

    DONE 3c, tinggal ordering

--- hw_3/p2/a2.py
+++ hw_3/p2/a2.py
@@ -19,7 +19,11 @@ def load_news_data(filepath):
 
 def create_dictionary(filepath='news.vocab'):
     with open(filepath, 'r') as f:
-        list_of_words = f.readlines()
+        raw_list = f.readlines()
+
+    list_of_words = []
+    for elem in raw_list:
+        list_of_words.append(elem.strip('\n'))
 
     return list_of_words
 
@@ -129,15 +133,58 @@ def predict(feature_vectors, label_prob, word_prob):
     # Add the argmax index result by 1 to obtain the correct label
     return np.add(np.argmax(final_log_probs, axis=1), 1) # Shape: (n,)
 
-def compute_error_rate(test_data, test_labels, label_prob, word_prob):
-    pred_result = predict(test_data, label_prob, word_prob)
-
+def compute_error_rate(pred_result, labels):
     # We compute the error rate here, so wrong prediction will yield 1
     # and correct prediction will yield 0
-    pred_verdict = [1 if pred_result[i] != test_labels[i] else 0 for i in range(0, len(test_data))]
+    pred_verdict = [1 if pred_result[i] != labels[i] else 0 for i in range(0, len(labels))]
 
     # Sum the wrong predictions and divide it by total test data
     return np.sum(pred_verdict) / len(pred_verdict)
+
+# For binary experiment only
+def calculate_bias(label_prob, word_prob):
+    log_label_prob = np.log(label_prob)
+    label_prob_diff = log_label_prob[1] - log_label_prob[0]
+
+    one_minus_word_prob = np.add(np.multiply(word_prob, -1), 1)
+    log_one_minus_word_prob = np.log(one_minus_word_prob)
+    word_prob_diff = np.sum(log_one_minus_word_prob[1]) - np.sum(log_one_minus_word_prob[0])
+
+    return label_prob_diff + word_prob_diff
+
+# For binary experiment only
+def calculate_weights(word_prob):
+    one_minus_word_prob = np.add(np.multiply(word_prob, -1), 1)
+
+    log_word_prob = np.log(word_prob)
+    log_one_minus_word_prob = np.log(one_minus_word_prob)
+
+    return (log_word_prob[1] - log_word_prob[0]) - (log_one_minus_word_prob[1] - log_one_minus_word_prob[0])
+
+# For binary experiment only
+def predict_affine(feature_vectors, bias, weights):
+    wx = np.dot(feature_vectors, weights)
+    b = bias
+    y = wx + b
+
+    return [2 if pred > 0 else 1 for pred in y]
+
+# For binary experiment only
+def get_top_and_bottom_words(weights, word_list, k=20):
+    minus_k = (-1) * k
+
+    k_biggest_indexes = np.argpartition(weights, minus_k)[:minus_k]
+    k_smallest_indexes = np.argpartition(weights, k)[:k]
+
+    top_weights = np.take(weights, k_biggest_indexes)
+    bottom_weights = np.take(weights, k_smallest_indexes)
+
+    word_list = np.array(word_list)
+
+    top_words = np.take(word_list, k_biggest_indexes)
+    bottom_words = np.take(word_list, k_smallest_indexes)
+
+    return top_words, top_weights, bottom_words, bottom_weights
 
 # Experiment for 20 labels
 def experiment_3a():
@@ -164,15 +211,21 @@ def experiment_3a():
     print('Elapsed time: ' + str(time.time() - start))
     print()
 
+    # Prediction result on train data
+    train_pred_result = predict(train_data, label_prob, word_prob)
+
     # Calculate train_error_rate
-    train_error_rate = compute_error_rate(train_data, train_labels, label_prob, word_prob)
+    train_error_rate = compute_error_rate(train_pred_result, train_labels)
     print('Train error rate: ' + str(train_error_rate))
     print('Done calculating train error rate')
     print('Elapsed time: ' + str(time.time() - start))
     print()
 
+    # Prediction result on test data
+    test_pred_result = predict(test_data, label_prob, word_prob)
+
     # Calculate test_error_rate
-    test_error_rate = compute_error_rate(test_data, test_labels, label_prob, word_prob)
+    test_error_rate = compute_error_rate(test_pred_result, test_labels)
     print('Test error rate: ' + str(test_error_rate))
     print('Done calculating test error rate')
     print('Elapsed time: ' + str(time.time() - start))
@@ -212,15 +265,21 @@ def experiment_3b():
     print('Elapsed time: ' + str(time.time() - start))
     print()
 
+    # Prediction result on train data
+    train_pred_result = predict(train_data, label_prob, word_prob)
+
     # Calculate train_error_rate
-    train_error_rate = compute_error_rate(train_data, train_labels, label_prob, word_prob)
+    train_error_rate = compute_error_rate(train_pred_result, train_labels)
     print('Train error rate: ' + str(train_error_rate))
     print('Done calculating train error rate')
     print('Elapsed time: ' + str(time.time() - start))
     print()
 
+    # Prediction result on test data
+    test_pred_result = predict(test_data, label_prob, word_prob)
+
     # Calculate test_error_rate
-    test_error_rate = compute_error_rate(test_data, test_labels, label_prob, word_prob)
+    test_error_rate = compute_error_rate(test_pred_result, test_labels)
     print('Test error rate: ' + str(test_error_rate))
     print('Done calculating test error rate')
     print('Elapsed time: ' + str(time.time() - start))
@@ -229,7 +288,83 @@ def experiment_3b():
     print('----------------------------------------')
     print()
 
-if __name__ == '__main__':
-    experiment_3a()
+# Experiment for binary labels with affine function
+def experiment_3c():
+    start = time.time()
 
-    experiment_3b()
+    print('Experiment 3c: ')
+    print()
+
+    # Get the data
+    train_data, train_labels, test_data, test_labels = load_news_data('news_binary.mat')
+
+    # Modify label from (-1, 1) to (1, 2) so it's easier to process
+    # as it follows the previous convention that I use: label = index + 1
+    train_labels = np.where(train_labels == -1, 1, 2)
+    test_labels = np.where(test_labels == -1, 1, 2)
+
+    # Create word vocab list
+    word_list = create_dictionary()
+
+    # Calculate pi_y, i.e. the label probability
+    label_count, label_prob = calculate_label_count_and_probability(train_labels)
+
+    # Sanity check (probabilities sum up to 1)
+    assert check_sum_probability(label_prob)
+
+    # Calculate miu_y_j, i.e. the word probability
+    # The difference from experiment 3a is than the train_labels is not sorted
+    word_prob = calculate_word_given_label_prob(train_data, train_labels, label_count, word_list, False)
+    print('Done calculating word prob')
+    print('Elapsed time: ' + str(time.time() - start))
+    print()
+
+    bias = calculate_bias(label_prob, word_prob)
+    weights = calculate_weights(word_prob)
+    print('Done calculating bias and weights')
+    print('Elapsed time: ' + str(time.time() - start))
+    print()
+
+    # Prediction result on train data
+    train_pred_result = predict_affine(train_data, bias, weights)
+
+    # Calculate train_error_rate
+    train_error_rate = compute_error_rate(train_pred_result, train_labels)
+    print('Train error rate: ' + str(train_error_rate))
+    print('Done calculating train error rate')
+    print('Elapsed time: ' + str(time.time() - start))
+    print()
+
+    # Prediction result on test data
+    test_pred_result = predict_affine(test_data, bias, weights)
+
+    # Calculate test_error_rate
+    test_error_rate = compute_error_rate(test_pred_result, test_labels)
+    print('Test error rate: ' + str(test_error_rate))
+    print('Done calculating test error rate')
+    print('Elapsed time: ' + str(time.time() - start))
+    print()
+
+    top_words, top_weights, bottom_words, bottom_weights = get_top_and_bottom_words(weights, word_list)
+    print('Top words:')
+    print(top_words)
+    print()
+    print('Top weights:')
+    print(top_weights)
+    print()
+    print('Bottom words:')
+    print(bottom_words)
+    print()
+    print('Bottom weights:')
+    print(bottom_weights)
+    print()
+
+    print('----------------------------------------')
+    print()
+
+if __name__ == '__main__':
+    # experiment_3a()
+    #
+    # experiment_3b()
+
+    experiment_3c()
