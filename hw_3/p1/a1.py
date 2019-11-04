8435b057ffb9e12518aa03fe0a6c37a9e89a32fa
geraldzakwan <geraldi.dzakwan@gmail.com>
2019-11-04 16:56:28 -0500

    Cleared 1b

--- hw_3/p1/a1.py
+++ hw_3/p1/a1.py
@@ -21,21 +21,6 @@ def transform_feature(one_dim_feature_vector):
 
     return transformed_feature_vector
 
-def create_weights_diagonal_matrix(squared=False):
-    # Shape (256,)
-    weights = np.zeros(2*128)
-
-    for j in range(0, 128):
-        weights[2*j] = np.ceil((j+1)/2)
-        if squared:
-            weights[2*j] = weights[2*j] ** 2
-
-        weights[(2*j)+1] = np.ceil((j+1)/2)
-        if squared:
-            weights[(2*j)+1] = weights[(2*j)+1] ** 2
-
-    return weights, np.diag(weights)
-
 # Use np.linalg.lstsq to compute the minimum Euclidean norm solution
 def compute_min_euclidean_norm_solution(feature_matrix, label_vector):
     # Get X'
@@ -49,6 +34,20 @@ def compute_min_euclidean_norm_solution(feature_matrix, label_vector):
     # M_sol fulfills the equation: np.dot(M_1, M_sol) = M_2
     # In our case, M_1 would be X'X, M_2 would be X'y and M_sol would be the weights we seek
     return np.linalg.lstsq(feature_matrix_matmul, np.dot(feature_matrix_transposed, label_vector), rcond=None)[0]
+
+def create_weights_diagonal_matrix(squared=False):
+    weights = np.zeros(2*128)
+
+    for j in range(0, 128):
+        weights[2*j] = np.ceil((j+1)/2)
+        if squared:
+            weights[2*j] = weights[2*j] ** 2
+
+        weights[(2*j)+1] = np.ceil((j+1)/2)
+        if squared:
+            weights[(2*j)+1] = weights[(2*j)+1] ** 2
+
+    return np.diag(weights)
 
 def run_experiment_1a():
     train_df = load_csv_to_ndarray('hw3p1_train.csv')
@@ -69,10 +68,45 @@ def run_experiment_1a():
 
     # fig = plt.figure(figsize=(10, 10))
     #
-    # plt.title('Min Euclidean Plot')
+    # plt.title('Minimum Euclidean Plot')
+    # plt.plot(test_df, test_pred_result, color='pink', linewidth=1, marker='o', markersize=3, mfc='white', mec='black')
+    #
+    # plt.savefig('1a.png', bbox_inches='tight')
+
+def run_experiment_1b():
+    train_df = load_csv_to_ndarray('hw3p1_train.csv')
+    test_df = load_csv_to_ndarray('hw3p1_test.csv')
+
+    one_dim_feature_vector = train_df[:,0]
+    label_vector = train_df[:,1]
+
+    feature_matrix = transform_feature(one_dim_feature_vector)
+
+    weights_diagonal_matrix = create_weights_diagonal_matrix()
+
+    # Stack feature_matrix with weights_diagonal
+    weighted_feature_matrix = np.vstack((feature_matrix, weights_diagonal_matrix))
+
+    # Pad label_vector with zeros
+    padded_label_vector = np.zeros(2*128 + len(label_vector))
+    padded_label_vector[0:len(label_vector)] = label_vector
+
+    sol = compute_min_euclidean_norm_solution(weighted_feature_matrix, padded_label_vector)
+
+    test_feature_matrix = transform_feature(test_df)
+    test_pred_result = np.dot(test_feature_matrix, sol)
+
+    # By default, Mac OS X can't directly render matplotlib
+    # To use matplotlib, please use Jupyter Notebook or set the backend properly (https://stackoverflow.com/questions/21784641/installation-issue-with-matplotlib-python)
+
+    # fig = plt.figure(figsize=(10, 10))
+    #
+    # plt.title('Minimum Weighted Euclidean Plot')
     # plt.plot(test_df, test_pred_result, color='pink', linewidth=1, marker='o', markersize=3, mfc='white', mec='black')
     #
     # plt.savefig('1a.png', bbox_inches='tight')
 
 if __name__ == '__main__':
     run_experiment_1a()
+
+    run_experiment_1b()
