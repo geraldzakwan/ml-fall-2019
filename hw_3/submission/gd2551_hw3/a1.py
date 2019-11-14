2de9ccaf7de34fa0407e53c7a0e7cefc5a3c7611
geraldzakwan <geraldi.dzakwan@gmail.com>
2019-11-14 16:12:49 -0500

    Last HW3

---
+++ hw_3/submission/gd2551_hw3/a1.py
@@ -0,0 +1,300 @@
+import numpy as np
+import scipy
+from scipy import optimize
+
+def load_csv_to_ndarray(filepath):
+    # Read using numpy lib and return all rows, no header in this case
+    return np.genfromtxt(filepath, dtype=float, delimiter=',')
+
+# Transform 1d feature to 256d using the trigonometric feature map/expansion
+def transform_feature(one_dim_feature_vector, weighted=False):
+    transformed_feature_vector = np.zeros((len(one_dim_feature_vector), 2*128))
+
+    for i in range(0, len(one_dim_feature_vector)):
+        feature = one_dim_feature_vector[i]
+        transformed_feature = np.zeros(2*128)
+
+        for j in range(0, 128):
+            transformed_feature[2*j] = np.cos((j+1) * feature)
+            if weighted:
+                transformed_feature[2*j] = transformed_feature[2*j] * np.ceil(((2*j)+1)/2)
+
+            transformed_feature[(2*j)+1] = np.sin((j+1) * feature)
+            if weighted:
+                transformed_feature[(2*j)+1] = transformed_feature[(2*j)+1] * np.ceil(((2*j)+2)/2)
+
+        transformed_feature_vector[i] = transformed_feature
+
+    return transformed_feature_vector
+
+# Use either np.linalg.pinv or np.linalg.lstsq to compute the minimum Euclidean norm solution
+# Both yield the same results
+# The default method used is the Moore Pseudo inverse as it can accommodate all types of matrices,
+# including those that don't have full row rank
+def compute_min_euclidean_norm_solution(feature_matrix, label_vector, use_pinv=True):
+    if use_pinv:
+        print('USE PINV')
+        pseudo_inverse = np.linalg.pinv(feature_matrix)
+        return np.dot(pseudo_inverse, label_vector)
+
+    print('USE LSTSQ')
+    # Get X'
+    feature_matrix_transposed = feature_matrix.T
+
+    # Compute X'X (covariance matrix)
+    feature_matrix_matmul = np.dot(feature_matrix_transposed, feature_matrix)
+
+    # Basically, lstsq(M_1, M_2) will return a solution, let say M_sol
+    # M_sol has the least squares or l2 norms amongst other solutions (there can also be no solution)
+    # M_sol fulfills the equation: np.dot(M_1, M_sol) = M_2
+    # In our case, M_1 would be X'X, M_2 would be X'y and M_sol would be the weights we seek
+    return np.linalg.lstsq(feature_matrix_matmul, np.dot(feature_matrix_transposed, label_vector), rcond=None)[0]
+
+# Create weights of size 256 as described in the problem: [ceil(1/2), ceil(2/2), ceil(3/2), ceil(4/2), ...]
+# and a diagonal matrix that represents the weights
+def create_weights_diagonal_matrix():
+    weights = np.zeros(2*128)
+
+    for j in range(0, 2*128):
+        weights[j] = np.ceil((j+1)/2)
+
+    return weights, np.diag(weights)
+
+# Count nonzero entries in the solution with threshold = 1.e-15,
+# i.e. values that are less than the threshold are treated as zero
+def count_nonzero_entries(weights, threshold=1.e-15):
+    count = 0
+
+    for weight in weights:
+        if abs(weight) < threshold:
+            count = count + 1
+
+    return count
+
+# Calculate squared loss for debugging purposes
+def calculate_squared_loss(prediction_values, label_vector, mean=True):
+    # Calculate sigma (y_hat - y)^2
+    total_squared_loss = np.sum((prediction_values - label_vector) ** 2)
+
+    # Divide error by total observations to get mean squared loss
+    if mean:
+        return total_squared_loss / len(label_vector)
+
+    return total_squared_loss
+
+# Main function for experiment 1a
+def compute_w_euclid(use_pinv=True):
+    print('Experiment 1a: ')
+    print()
+
+    train_df = load_csv_to_ndarray('hw3p1_train.csv')
+    test_df = load_csv_to_ndarray('hw3p1_test.csv')
+
+    one_dim_feature_vector = train_df[:,0]
+    label_vector = train_df[:,1]
+
+    feature_matrix = transform_feature(one_dim_feature_vector)
+
+    sol = compute_min_euclidean_norm_solution(feature_matrix, label_vector, use_pinv)
+    print('First 16 elements: ')
+    print(sol[0:16])
+    print()
+    print('Last 16 elements: ')
+    print(sol[240:256])
+    print()
+
+    print('Norm:')
+    print(np.linalg.norm(sol))
+    print()
+
+    test_feature_matrix = transform_feature(test_df)
+    test_pred_result = np.dot(test_feature_matrix, sol)
+
+    print('Mean squared loss: ')
+    print(calculate_squared_loss(np.dot(feature_matrix, sol), label_vector))
+    print()
+
+    print('Number of nonzero entries:')
+    print(count_nonzero_entries(sol))
+    print()
+
+    print('--------------------------------')
+
+    # # By default, Mac OS X can't directly render matplotlib
+    # # To use matplotlib, please use Jupyter Notebook or set the backend properly (https://stackoverflow.com/questions/21784641/installation-issue-with-matplotlib-python)
+    # import matplotlib.pyplot as plt
+    #
+    # fig = plt.figure(figsize=(10, 10))
+    #
+    # plt.title('Minimum Euclidean Plot')
+    # plt.plot(test_df, test_pred_result, color='pink', linewidth=1, marker='o', markersize=3, mfc='white', mec='black')
+    #
+    # plt.savefig('1a.png', bbox_inches='tight')
+    #
+    # plt.show()
+
+def compute_w_weighted(use_pinv=True):
+    print('Experiment 1b: ')
+    print()
+
+    train_df = load_csv_to_ndarray('hw3p1_train.csv')
+    test_df = load_csv_to_ndarray('hw3p1_test.csv')
+
+    one_dim_feature_vector = train_df[:,0]
+    label_vector = train_df[:,1]
+
+    feature_matrix = transform_feature(one_dim_feature_vector)
+
+    _, weights_diagonal_matrix = create_weights_diagonal_matrix()
+
+    # Stack feature_matrix with diagonal matrix of weights
+    weighted_feature_matrix = np.vstack((feature_matrix, weights_diagonal_matrix))
+
+    # Pad label_vector with zeros to match the dimension of new feature_matrix
+    padded_label_vector = np.zeros(2*128 + len(label_vector))
+    padded_label_vector[0:len(label_vector)] = label_vector
+
+    sol = compute_min_euclidean_norm_solution(weighted_feature_matrix, padded_label_vector, use_pinv)
+    print('First 16 elements: ')
+    print(sol[0:16])
+    print()
+    print('Last 16 elements: ')
+    print(sol[240:256])
+    print()
+
+    print('Norm:')
+    print(np.linalg.norm(sol))
+    print()
+
+    test_feature_matrix = transform_feature(test_df)
+    test_pred_result = np.dot(test_feature_matrix, sol)
+
+    print('Mean squared loss: ')
+    print(calculate_squared_loss(np.dot(feature_matrix, sol), label_vector))
+    print()
+
+    print('Number of nonzero entries:')
+    print(count_nonzero_entries(sol))
+    print()
+
+    print('--------------------------------')
+
+    # # By default, Mac OS X can't directly render matplotlib
+    # # To use matplotlib, please use Jupyter Notebook or set the backend properly (https://stackoverflow.com/questions/21784641/installation-issue-with-matplotlib-python)
+    # import matplotlib.pyplot as plt
+    #
+    # fig = plt.figure(figsize=(10, 10))
+    #
+    # plt.title('Minimum Weighted Euclidean Plot')
+    # plt.plot(test_df, test_pred_result, color='pink', linewidth=1, marker='o', markersize=3, mfc='white', mec='black')
+    #
+    # plt.savefig('1b.png', bbox_inches='tight')
+    #
+    # plt.show()
+
+def compute_w_dantzig():
+    print('Experiment 1c: ')
+    print()
+
+    train_df = load_csv_to_ndarray('hw3p1_train.csv')
+    test_df = load_csv_to_ndarray('hw3p1_test.csv')
+
+    one_dim_feature_vector = train_df[:,0]
+    label_vector = train_df[:,1]
+
+    feature_matrix = transform_feature(one_dim_feature_vector)
+
+    # weights: [ceil(1/2), ceil(2,2), ...], size: 256
+    weights, _ = create_weights_diagonal_matrix()
+
+    # Size: 512
+    appended_weights = np.zeros(2*2*128)
+
+    # This is the linear objective function
+    # First 256 is for w, last 256 for v
+    # Give zero weights to w (first 256) because
+    # we care only about sum(abs(w)), i.e. sum(v)
+    appended_weights[2*128:2*2*128] = weights
+
+    # Equality constraint
+
+    # A^tA
+    A_eq = np.dot(feature_matrix.transpose(), feature_matrix)
+    # Add 256 more zero columns to fit for v
+    A_eq = np.hstack((A_eq, np.zeros((2*128, 2*128))))
+
+    # Label vector stay as is, A^tb will have shape 256
+    b_eq = np.dot(feature_matrix.transpose(), label_vector)
+
+    # Inequality constraint
+    # Each pair of v and w needs to follow: - w - v <= 0 and w - v <= 0
+    A_ub = np.zeros((512, 512))
+    for i in range(0, int(len(A_ub)/2)):
+        # -w_j - v_j
+        A_ub[2*i][i] = -1
+        A_ub[2*i][i+256] = -1
+
+        # w_j - v_j
+        A_ub[(2*i)+1][i] = 1
+        A_ub[(2*i)+1][i+256] = -1
+
+    # Upper bound for w and v
+    b_ub = np.zeros(512)
+
+    # Set bound for w from -inf to inf (None, None)
+    # Set bound for v from 0 to inf (0, None) -> absolute value, non-negative
+    bounds = []
+    for i in range(0, 256):
+        bounds.append((None, None))
+    for i in range(0, 256):
+        bounds.append((0, None))
+
+    # Call the linear programming solver with method revised simplex
+    res = scipy.optimize.linprog(appended_weights, A_eq=A_eq, b_eq=b_eq, A_ub=A_ub, b_ub=b_ub, bounds=bounds, method='revised simplex')
+
+    # Get the solution from result object
+    dantzig_sol = res.x[0:256]
+
+    print('First 16 elements: ')
+    print(dantzig_sol[0:16])
+    print()
+    print('Last 16 elements: ')
+    print(dantzig_sol[240:256])
+    print()
+
+    print('Norm:')
+    print(np.linalg.norm(dantzig_sol))
+    print()
+
+    print('Mean squared loss: ')
+    print(calculate_squared_loss(np.dot(feature_matrix, dantzig_sol), label_vector))
+    print()
+
+    print('Number of nonzero entries:')
+    print(count_nonzero_entries(dantzig_sol))
+    print()
+
+    print('--------------------------------')
+
+    # # By default, Mac OS X can't directly render matplotlib
+    # # To use matplotlib, please use Jupyter Notebook or set the backend properly (https://stackoverflow.com/questions/21784641/installation-issue-with-matplotlib-python)
+    # import matplotlib.pyplot as plt
+    #
+    # test_feature_matrix = transform_feature(test_df)
+    # test_pred_result = np.dot(test_feature_matrix, dantzig_sol)
+    #
+    # fig = plt.figure(figsize=(10, 10))
+    #
+    # plt.title('Dantzig Selector Euclidean Plot')
+    # plt.plot(test_df, test_pred_result, color='pink', linewidth=1, marker='o', markersize=3, mfc='white', mec='black')
+    #
+    # plt.savefig('1c.png', bbox_inches='tight')
+    #
+    # plt.show()
+
+if __name__ == '__main__':
+    compute_w_euclid()
+
+    compute_w_weighted()
+
+    compute_w_dantzig()
---
+++ hw_3/submission/gd2551_hw3/a2.py
@@ -0,0 +1,401 @@
+import time
+import numpy as np
+
+from collections import defaultdict
+from scipy.io import loadmat
+
+def load_news_data(filepath):
+    news = loadmat(filepath)
+
+    # From scipy csc matrix to 2D array
+    train_data = news['data'].toarray()
+    # From 2D array to 1D array
+    train_labels = news['labels'].flatten()
+
+    test_data = news['testdata'].toarray()
+    test_labels = news['testlabels'].flatten()
+
+    return train_data, train_labels, test_data, test_labels
+
+def create_dictionary(filepath='news.vocab'):
+    with open(filepath, 'r') as f:
+        raw_list = f.readlines()
+
+    list_of_words = []
+    for elem in raw_list:
+        list_of_words.append(elem.strip('\n'))
+
+    return list_of_words
+
+# This is going to be the pi_y, i.e. label probability
+def calculate_label_count_and_probability(labels):
+    # Count label occurrence, store in 1D array
+    # Set array size equals to the number of unique labels
+    label_count = np.zeros(len(np.unique(labels)))
+    for label in labels:
+        # The important thing is to set index to (label - 1)
+        # as index starts from zero but label starts from 1
+        label_count[label - 1] = label_count[label - 1] + 1
+
+    # Calculate label probability, store in 1D array
+    label_prob = np.zeros(len(label_count))
+    for label in labels:
+        # Again, index is label - 1
+        label_prob[label - 1] = label_count[label - 1] / len(labels)
+
+    return label_count, label_prob
+
+# This is going to be the miu_y_j
+# The idea is to calculate separately for each value of y, i.e.
+# getting train_data only for a particular value of y
+# Then, sum over the word index and divide by the number of data
+def calculate_word_given_label_prob(train_data, train_labels, label_count, word_list, is_sorted, laplace_smoothing=True):
+    # The return array of shape (20, 61188)
+    # i.e. (number of labels, word vocab size)
+    word_prob = np.zeros((len(label_count), len(word_list)))
+
+    # Iterate over label
+    for i in range(0, len(label_count)):
+        # For each label, find all indexes in train_labels that correspond
+        indexes, = np.where(train_labels == i + 1) # Again, label is index plus one
+
+        # Get the corresponding train_data for that label
+        if is_sorted:
+            # If the train_labels is sorted, we can just find two indexes
+            # where a particular label starts and ends
+            # Then, use that two indexes to slice the train data
+            # This is the case for 'news.mat'
+            first_idx = indexes[0]
+            last_idx = indexes[-1]
+
+            corr_train_data = train_data[first_idx:last_idx]
+        else:
+            # If the train_labels is not sorted, we need to use np.take
+            # to take all train_data that correspond to the label
+            # This is the case for 'news_binary.mat' and this is rather slow
+            corr_train_data = np.take(train_data, indexes, axis=0)
+
+        # Sum over axis=0, i.e. sum the word occurrence
+        word_sum = np.sum(corr_train_data, axis=0)
+
+        if laplace_smoothing:
+            # Laplace smoothing, add each sum by 1
+            word_sum = np.add(word_sum, 1)
+
+        # Finally, calculate the word prob for this particular label
+        if laplace_smoothing:
+            # Divide by label_count + 2 (Laplace smoothing)
+            word_prob_for_label = np.divide(word_sum, label_count[i] + 2)
+        else:
+            word_prob_for_label = np.divide(word_sum, label_count[i])
+
+        # Assign result to the return array
+        word_prob[i] = word_prob_for_label
+
+    return word_prob
+
+# Return True if array of probabilities sums up (closely) to 1
+def check_sum_probability(array, epsilon=0.000001):
+    return abs(np.sum(array) - 1.0) < epsilon
+
+# Input: Feature vectors, 2D array of shape (n, d),
+# where n is the number of data supplied and d is the dimension: 61188
+# Return 1D array of predicted labels (size n)
+def predict(feature_vectors, label_prob, word_prob):
+    # Create the term 1 - x_j
+    # Shape: (n, 61188)
+    one_minus_feature_vectors = np.add(np.multiply(feature_vectors, -1), 1)
+
+    # Create the term 1 - miu_y_j
+    # Shape: (20, 61188)
+    one_minus_word_prob = np.add(np.multiply(word_prob, -1), 1)
+
+    # Log values of all the components
+    pi_log_prob = np.log(label_prob) # Shape: (20,)
+    log_word_prob = np.log(word_prob) # Shape: (20, 61188)
+    log_one_minus_word_prob = np.log(one_minus_word_prob) # Shape: (20, 61188)
+
+    # Dot product of x_j and ln(miu_y_j)
+    # Shape: (n, 20)
+    dot_product_one = np.dot(feature_vectors, log_word_prob.transpose())
+
+    # Dot product of (1 - x_j) and ln(1 - miu_y_j)
+    # Shape: (n, 20)
+    dot_product_two = np.dot(one_minus_feature_vectors, log_one_minus_word_prob.transpose())
+
+    # pi_log_prob will be broadcasted automatically,
+    # i.e. from shape (20,) to (n, 20)
+    # Shape: (n, 20)
+    final_log_probs = dot_product_one + dot_product_two + pi_log_prob
+
+    # Finally, return the label that has maximum logprob in each row
+    # This is done by using argmax on axis=1
+    # Add the argmax index result by 1 to obtain the correct label
+    return np.add(np.argmax(final_log_probs, axis=1), 1) # Shape: (n,)
+
+def compute_error_rate(pred_result, labels):
+    # We compute the error rate here, so wrong prediction will yield 1
+    # and correct prediction will yield 0
+    pred_verdict = [1 if pred_result[i] != labels[i] else 0 for i in range(0, len(labels))]
+
+    # Sum the wrong predictions and divide it by total test data
+    return np.sum(pred_verdict) / len(pred_verdict)
+
+# For binary experiment only
+def calculate_bias(label_prob, word_prob):
+    log_label_prob = np.log(label_prob)
+    label_prob_diff = log_label_prob[1] - log_label_prob[0]
+
+    one_minus_word_prob = np.add(np.multiply(word_prob, -1), 1)
+    log_one_minus_word_prob = np.log(one_minus_word_prob)
+    word_prob_diff = np.sum(log_one_minus_word_prob[1]) - np.sum(log_one_minus_word_prob[0])
+
+    return label_prob_diff + word_prob_diff
+
+# For binary experiment only
+def calculate_weights(word_prob):
+    one_minus_word_prob = np.add(np.multiply(word_prob, -1), 1)
+
+    log_word_prob = np.log(word_prob)
+    log_one_minus_word_prob = np.log(one_minus_word_prob)
+
+    return (log_word_prob[1] - log_word_prob[0]) - (log_one_minus_word_prob[1] - log_one_minus_word_prob[0])
+
+# For binary experiment only
+def predict_affine(feature_vectors, bias, weights):
+    wx = np.dot(feature_vectors, weights)
+    b = bias
+    y = wx + b
+
+    return [2 if pred > 0 else 1 for pred in y]
+
+# For binary experiment only
+def get_top_and_bottom_words(weights, word_list, k=20):
+    minus_k = (-1) * k
+
+    k_biggest_indexes = np.argpartition(weights, minus_k)[minus_k:]
+    # k_biggest_indexes_slower = (-weights).argsort()[:k]
+
+    # print(np.sort(k_biggest_indexes))
+    # print(np.sort(k_biggest_indexes_slower))
+
+    k_smallest_indexes = np.argpartition(weights, k)[:k]
+
+    top_weights = np.take(weights, k_biggest_indexes)
+    bottom_weights = np.take(weights, k_smallest_indexes)
+
+    word_list = np.array(word_list)
+
+    top_words = np.take(word_list, k_biggest_indexes)
+    bottom_words = np.take(word_list, k_smallest_indexes)
+
+    # Zip vocab_index, word and their weight
+    zipped_top = [(k_biggest_indexes[i], top_words[i], top_weights[i]) for i in range(0, len(top_weights))]
+    zipped_bottom = [(k_smallest_indexes[i], bottom_words[i], bottom_weights[i]) for i in range(0, len(bottom_weights))]
+
+    # Return them in descending order of their absolute weight value
+    zipped_top = sorted(zipped_top, key=lambda x: x[2], reverse=True)
+    zipped_bottom = sorted(zipped_bottom, key=lambda x: x[2])
+
+    return zipped_top, zipped_bottom
+
+# Experiment for 20 labels
+def run_experiment_2a():
+    start = time.time()
+
+    print('Experiment 2a: ')
+    print()
+
+    # Get the data
+    train_data, train_labels, test_data, test_labels = load_news_data('news.mat')
+
+    # Create word vocab list
+    word_list = create_dictionary()
+
+    # Calculate pi_y, i.e. the label probability
+    label_count, label_prob = calculate_label_count_and_probability(train_labels)
+
+    # Sanity check (probabilities sum up to 1)
+    assert check_sum_probability(label_prob)
+
+    # Calculate miu_y_j, i.e. the word probability
+    word_prob = calculate_word_given_label_prob(train_data, train_labels, label_count, word_list, True)
+    print('Done calculating word prob')
+    print('Elapsed time: ' + str(time.time() - start) + ' seconds')
+    print()
+
+    # Prediction result on train data
+    train_pred_result = predict(train_data, label_prob, word_prob)
+
+    # Calculate train_error_rate
+    train_error_rate = compute_error_rate(train_pred_result, train_labels)
+    print('Train error rate: ' + str(train_error_rate))
+    print('Done calculating train error rate')
+    print('Elapsed time: ' + str(time.time() - start) + ' seconds')
+    print()
+
+    # Prediction result on test data
+    test_pred_result = predict(test_data, label_prob, word_prob)
+
+    # Calculate test_error_rate
+    test_error_rate = compute_error_rate(test_pred_result, test_labels)
+    print('Test error rate: ' + str(test_error_rate))
+    print('Done calculating test error rate')
+    print('Elapsed time: ' + str(time.time() - start) + ' seconds')
+    print()
+
+    print('----------------------------------------')
+    print()
+
+# Experiment for binary labels
+def run_experiment_2b():
+    start = time.time()
+
+    print('Experiment 2b: ')
+    print()
+
+    # Get the data
+    train_data, train_labels, test_data, test_labels = load_news_data('news_binary.mat')
+
+    # Modify label from (-1, 1) to (1, 2) so it's easier to process
+    # as it follows the previous convention that I use: label = index + 1
+    train_labels = np.where(train_labels == -1, 1, 2)
+    test_labels = np.where(test_labels == -1, 1, 2)
+
+    # Create word vocab list
+    word_list = create_dictionary()
+
+    # Calculate pi_y, i.e. the label probability
+    label_count, label_prob = calculate_label_count_and_probability(train_labels)
+
+    # Sanity check (probabilities sum up to 1)
+    assert check_sum_probability(label_prob)
+
+    # Calculate miu_y_j, i.e. the word probability
+    # The difference from experiment 2a is than the train_labels is not sorted
+    word_prob = calculate_word_given_label_prob(train_data, train_labels, label_count, word_list, False)
+    print('Done calculating word prob')
+    print('Elapsed time: ' + str(time.time() - start) + ' seconds')
+    print()
+
+    # Prediction result on train data
+    train_pred_result = predict(train_data, label_prob, word_prob)
+
+    # Calculate train_error_rate
+    train_error_rate = compute_error_rate(train_pred_result, train_labels)
+    print('Train error rate: ' + str(train_error_rate))
+    print('Done calculating train error rate')
+    print('Elapsed time: ' + str(time.time() - start) + ' seconds')
+    print()
+
+    # Prediction result on test data
+    test_pred_result = predict(test_data, label_prob, word_prob)
+
+    # Calculate test_error_rate
+    test_error_rate = compute_error_rate(test_pred_result, test_labels)
+    print('Test error rate: ' + str(test_error_rate))
+    print('Done calculating test error rate')
+    print('Elapsed time: ' + str(time.time() - start) + ' seconds')
+    print()
+
+    print('----------------------------------------')
+    print()
+
+# Experiment for binary labels with affine function
+def run_experiment_2c():
+    start = time.time()
+
+    print('Experiment 2c: ')
+    print()
+
+    # Get the data
+    train_data, train_labels, test_data, test_labels = load_news_data('news_binary.mat')
+
+    # Modify label from (-1, 1) to (1, 2) so it's easier to process
+    # as it follows the previous convention that I use: label = index + 1
+    train_labels = np.where(train_labels == -1, 1, 2)
+    test_labels = np.where(test_labels == -1, 1, 2)
+
+    # Create word vocab list
+    word_list = create_dictionary()
+
+    # Calculate pi_y, i.e. the label probability
+    label_count, label_prob = calculate_label_count_and_probability(train_labels)
+
+    # Sanity check (probabilities sum up to 1)
+    assert check_sum_probability(label_prob)
+
+    # Calculate miu_y_j, i.e. the word probability
+    # The difference from experiment 2a is than the train_labels is not sorted
+    word_prob = calculate_word_given_label_prob(train_data, train_labels, label_count, word_list, False)
+    print('Done calculating word prob')
+    print('Elapsed time: ' + str(time.time() - start) + ' seconds')
+    print()
+
+    bias = calculate_bias(label_prob, word_prob)
+    weights = calculate_weights(word_prob)
+    print('Done calculating bias and weights')
+    print('Elapsed time: ' + str(time.time() - start) + ' seconds')
+    print()
+
+    # Prediction result on train data
+    train_pred_result = predict_affine(train_data, bias, weights)
+
+    # Calculate train_error_rate
+    train_error_rate = compute_error_rate(train_pred_result, train_labels)
+    print('Train error rate: ' + str(train_error_rate))
+    print('Done calculating train error rate')
+    print('Elapsed time: ' + str(time.time() - start) + ' seconds')
+    print()
+
+    # Prediction result on test data
+    test_pred_result = predict_affine(test_data, bias, weights)
+
+    # Calculate test_error_rate
+    test_error_rate = compute_error_rate(test_pred_result, test_labels)
+    print('Test error rate: ' + str(test_error_rate))
+    print('Done calculating test error rate')
+    print('Elapsed time: ' + str(time.time() - start) + ' seconds')
+    print()
+
+    # Determine the most positive and negative words, 20 each
+    zipped_top, zipped_bottom = get_top_and_bottom_words(weights, word_list)
+    print('Most positive words, descendingly ordered based on the absolute weight value:')
+    for tup in zipped_top:
+        print(tup)
+    print()
+
+    # # Latex print
+    # it = 0
+    # for tup in zipped_top:
+    #     it = it + 1
+    #     print(str(it) + ' & ' + str(tup[0]) + ' & ' + str(tup[1]) + ' & ' + str(tup[2]) + '\\' + '\\')
+    #     print('\hline')
+    # print()
+
+    print('Most negative words, descendingly ordered based on the absolute weight value:')
+    for tup in zipped_bottom:
+        print(tup)
+    print()
+
+    # # Latex print
+    # it = 0
+    # for tup in zipped_bottom:
+    #     it = it + 1
+    #     print(str(it) + ' & ' + str(tup[0]) + ' & ' + str(tup[1]) + ' & ' + str(tup[2]) + '\\' + '\\')
+    #     print('\hline')
+    # print()
+
+    print('Done fetching the most positive and negative words')
+    print('Elapsed time: ' + str(time.time() - start) + ' seconds')
+    print()
+
+    print('----------------------------------------')
+    print()
+
+if __name__ == '__main__':
+    run_experiment_2a()
+
+    run_experiment_2b()
+
+    run_experiment_2c()
